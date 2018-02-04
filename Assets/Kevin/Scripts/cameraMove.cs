cc162ab681cfb2e8d0094bcfa4adb9173a35530c
Kevin De La Torre <ktorre@cpp.edu>
2018-02-03 21:48:09 -0800

    next commit

--- Assets/Kevin/Scripts/cameraMove.cs
+++ Assets/Kevin/Scripts/cameraMove.cs
@@ -9,17 +9,17 @@ public class cameraMove : MonoBehaviour {
 	void Start () {
         transform.rotation = Quaternion.identity;
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void LateUpdate () {
         if (Input.GetKey(KeyCode.LeftArrow))
             transform.position -= new Vector3(speed * Time.deltaTime, 0, 0);
         else if (Input.GetKey(KeyCode.RightArrow))
             transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
         else if (Input.GetKey(KeyCode.UpArrow))
-            transform.position += new Vector3(0, 0, speed * Time.deltaTime );
-        else if (Input.GetKey(KeyCode.DownArrow))
-            transform.position -= new Vector3(0, 0, speed * Time.deltaTime );
+            transform.position = transform.position + (transform.forward * speed * Time.deltaTime);
+        else if (Input.GetKey(KeyCode.DownArrow) )
+            transform.position = transform.position - (transform.forward * speed * Time.deltaTime);
 
 	}   
 }
--- Assets/OSPNative/scenes/Standard Assets/Character Controllers/Sources/Scripts/MouseLook.cs
+++ Assets/OSPNative/scenes/Standard Assets/Character Controllers/Sources/Scripts/MouseLook.cs
@@ -28,30 +28,45 @@ public class MouseLook : MonoBehaviour {
 	public float minimumY = -60F;
 	public float maximumY = 60F;
 
+    public bool camMov = false;
+
 	float rotationY = 0F;
 
-	void Update ()
+    private void OnMouseDown()
+    {
+        camMov = true;
+    }
+
+    private void OnMouseUp()
+    {
+        camMov = false;
+    }
+
+    void Update ()
 	{
-		if (axes == RotationAxes.MouseXAndY)
-		{
-			float rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
-			
-			rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
-			rotationY = Mathf.Clamp (rotationY, minimumY, maximumY);
-			
-			transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
-		}
-		else if (axes == RotationAxes.MouseX)
-		{
-			transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityX, 0);
-		}
-		else
-		{
-			rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
-			rotationY = Mathf.Clamp (rotationY, minimumY, maximumY);
-			
-			transform.localEulerAngles = new Vector3(-rotationY, transform.localEulerAngles.y, 0);
-		}
+        if (camMov)
+        {
+            if (axes == RotationAxes.MouseXAndY)
+            {
+                float rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
+
+                rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+                rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
+
+                transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
+            }
+            else if (axes == RotationAxes.MouseX)
+            {
+                transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityX, 0);
+            }
+            else
+            {
+                rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+                rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
+
+                transform.localEulerAngles = new Vector3(-rotationY, transform.localEulerAngles.y, 0);
+            }
+        }
 	}
 	
 	void Start ()
